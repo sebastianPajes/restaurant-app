91daded3fdad662a21e55c32a537f346645c1269
Sebastian Pajes <sebastian.pajes@ext.blaze.me>
2022-11-20 17:44:35 -0500

    amplify auth test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Amplify, I18n } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+
+import awsconfig from './aws-config';
+
+I18n.setLanguage('es');
+const dict = {
+  es: {
+    'Sign in': 'Registrarse',
+    'Forgot your password?': 'Olvido su contraseña?',
+    'Enter your Email': 'Ingrese su email',
+    'Enter your Password': 'Ingrese su password'
+  }
+};
+
+I18n.putVocabularies(dict);
+
+Amplify.configure(awsconfig);
+
+
+// export declare type FormFieldComponents = 'signIn' | 'signUp' | 'forceNewPassword' | 'confirmResetPassword' | 'confirmSignIn' | 'confirmSignUp' | 'confirmVerifyUser' | 'resetPassword' | 'setupTOTP';
+
+// const formFields = {
+//   signIn: {
+//     username: {
+//       placeholder: 'Ingrese su email',
+//     },
+//   },
+// }
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator 
+    // formFields={formFields}
+    loginMechanisms={['email']}
+    hideSignUp={true} >
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
